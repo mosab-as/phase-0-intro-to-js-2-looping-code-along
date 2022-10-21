6fabf947a89a17e7a40798d8132f6a98ea43c71e
Golden-Man <Golden-Man@users.noreply.github.com>
2022-10-21 16:40:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names,message)
+{
+    const result = [];
+    for(let i=0;i<names.length;i++)
+    {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+    }
+    return result;
+}
+
+function countDown(count)
+{
+    for(let i =count; i>=0;i--)
+    {
+        console.log(i)
+    }
+}
